7393c506f9db400fe616b53b3f61b9bf5228a262
Benjamin-Samuel Schlüter <benjamin-samuel.schluter@uclouvain.be>
2023-08-03 11:17:23 -0400

    simplify the website

--- code_post_creation.R
+++ code_post_creation.R
@@ -6,7 +6,7 @@ library(distill)
 
 # Don't put ¨ on the u --> creates bug
 create_post(
-        "Advises to starting PhD", # mandatory
+        "Uncertain about interpreting uncertainty?", # mandatory
         author = "Benjamin Schluter",
         date = Sys.Date(),
         date_prefix = Sys.Date(),
